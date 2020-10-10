82aaa4d94d595d43541360369068cab8e3362dea
Yusuke Gunji <inochideutae@gmail.com>
2020-10-10 16:53:44 +0900

    feat(home):homeモジュール追加

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { CardComponent } from './card/card.component';
 import { CreateDialogComponent } from './create-dialog/create-dialog.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [CardComponent, CreateDialogComponent],
+  declarations: [
+    CardComponent,
+    CreateDialogComponent,
+    CardListComponent,
+    HomeComponent,
+  ],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
