e86e3995d912724fa5005799be49925e31ff0d24
Yusuke Gunji <inochideutae@gmail.com>
2020-10-10 12:19:37 +0900

    feat(detail): detailモジュール作成

---
+++ src/app/detail/detail-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProjectDetailComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class DetailRoutingModule {}
