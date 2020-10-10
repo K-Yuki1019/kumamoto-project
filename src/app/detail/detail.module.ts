a38975093ccb5b14b55b5b8ccc20ce6fbbd45566
komura <affirman03139@outlook.jp>
2020-10-10 12:58:10 +0900

    feat(comment): コメントコンポーネント追加

--- src/app/detail/detail.module.ts
+++ src/app/detail/detail.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DetailRoutingModule } from './detail-routing.module';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectCommentComponent } from './project-comment/project-comment.component';
 
 @NgModule({
-  declarations: [ProjectDetailComponent],
+  declarations: [ProjectDetailComponent, ProjectCommentComponent],
   imports: [CommonModule, DetailRoutingModule],
 })
 export class DetailModule {}
---
+++ src/app/detail/project-comment/project-comment.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-comment',
+  templateUrl: './project-comment.component.html',
+  styleUrls: ['./project-comment.component.scss'],
+})
+export class ProjectCommentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
