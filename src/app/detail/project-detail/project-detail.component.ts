b83eadacc4e0587d74f953046199830725aca3ac
Yusuke Gunji <inochideutae@gmail.com>
2020-10-11 11:52:06 +0900

    feat(project-detail): いったん詳細画面追加

--- src/app/detail/project-detail/project-detail.component.ts
+++ src/app/detail/project-detail/project-detail.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { Project } from 'src/app/interfaces/project';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { Project, ProjectWithUser } from 'src/app/interfaces/project';
 import { UserData } from 'src/app/interfaces/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
   selector: 'app-project-detail',
@@ -11,9 +14,19 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class ProjectDetailComponent implements OnInit {
   user$: Observable<UserData> = this.authService.user$;
-  project$: Observable<Project>;
 
-  constructor(public authService: AuthService) {}
+  project$: Observable<ProjectWithUser> = this.route.paramMap.pipe(
+    switchMap((param) => {
+      const id = param.get('id');
+      return this.projectService.getProjectWithUserByProjectId(id);
+    })
+  );
+
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private projectService: ProjectService
+  ) {}
 
   ngOnInit(): void {}
 
